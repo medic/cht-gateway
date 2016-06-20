daf943aeb3610b2be0842dae01795f89e8bd82e9
Alex Anderson <anderson@medicmobile.org>
2016-06-20 13:37:55 +0200

    Add outline implementation of HeadlessSmsSendService
    This should prevent medic-gateway/device crashing if a RESPOND_VIA_MESSAGE
    intent is triggered.  The new service does nothing except log that the intent
    was fired.

---
+++ src/main/java/medic/gateway/alert/HeadlessSmsSendService.java
@@ -0,0 +1,21 @@
+package medic.gateway.alert;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import static medic.gateway.alert.GatewayLog.logEvent;
+
+public class HeadlessSmsSendService extends IntentService {
+	private static final String CLASS_NAME = HeadlessSmsSendService.class.getName();
+
+	public HeadlessSmsSendService() {
+		super(CLASS_NAME);
+	}
+
+	protected void onHandleIntent(Intent i) {
+		logEvent(this, "HeadlessSmsSendService :: received intent.  No action will be taken (none implemented) [Intent: action=%s, data=%s, msg=%s]",
+				i.getAction(),
+				i.getDataString(),
+				i.getStringExtra(Intent.EXTRA_TEXT));
+	}
+}
