666b5e50e673c23b4df507db8546a6561d92d402
Alex Anderson <anderson@medicmobile.org>
2016-06-21 12:59:25 +0200

    Add documentation to AlarmListener.scheduleAlarms()

--- src/main/java/medic/gateway/alert/AlarmListener.java
+++ src/main/java/medic/gateway/alert/AlarmListener.java
@@ -11,6 +11,11 @@ public class AlarmListener implements WakefulIntentService.AlarmListener {
 	public void scheduleAlarms(AlarmManager am, PendingIntent pendingIntent, Context ctx) {
 		if(Settings.in(ctx).isPollingEnabled()) {
 			logEvent(ctx, "AlarmManager.scheduleAlarms() :: polling enabled - setting alarms");
+
+			// On SDK >= 19, setRepeating will be inexact - the OS will try to fit alarms in with other
+			// activity which wakes the device.  This should be better for battery life, and -seems-
+			// acceptable.  However, testing across a range of devices may prove that it is simpler
+			// to use setWindow(), and reschedule the alarm each time it fires.
 			am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), getPollInterval(ctx), pendingIntent);
 		} else {
 			logEvent(ctx, "AlarmManager.scheduleAlarms() :: polling disabled - cancelling alarms");
