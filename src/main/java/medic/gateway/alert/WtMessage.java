681a5ae18d52771a0267368c1698ad9f316191c6
Estelle Comment <estelle@medicmobile.org>
2017-03-30 15:45:15 +0545

    Add WT StatusUpdate

--- src/main/java/medic/gateway/alert/WtMessage.java
+++ src/main/java/medic/gateway/alert/WtMessage.java
@@ -13,6 +13,37 @@ class WtMessage {
 		boolean canBeRetried() { return this == FAILED; }
 	}
 
+	public static class StatusUpdate {
+		public final long id;
+		public final String messageId;
+		public final Status newStatus;
+		public final long timestamp;
+		public StatusUpdate(long id, String messageId, Status newStatus, long timestamp) {
+			this.id = id;
+			this.messageId = messageId;
+			this.newStatus = newStatus;
+			this.timestamp = timestamp;
+		}
+		public String toString() {
+			return String.format("%s@%s-%s-%s", getClass().getSimpleName(), messageId, newStatus, timestamp);
+		}
+		public int hashCode() {
+			int p = 92821;
+			int h = 1;
+			h = h * p + (int) id;
+			return h;
+		}
+		public boolean equals(Object _that) {
+			if(this == _that) return true;
+			if(!(_that instanceof StatusUpdate)) return false;
+			StatusUpdate that = (StatusUpdate) _that;
+			return this.id == that.id &&
+					this.messageId == null ? that.messageId == null : this.messageId.equals(that.messageId) &&
+					this.newStatus == that.newStatus &&
+					this.timestamp == that.timestamp;
+		}
+	}
+
 	public final String id;
 	private Status status;
 	private long lastAction;
