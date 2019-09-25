2d85b141286ef616651588b6c03b78562ac2aaa7
Peter Ochieng <peteroochieng@gmail.com>
2019-09-25 12:24:18 +0300

    checkstyle changes

--- src/androidTest/java/medic/gateway/alert/WakefulServiceTest.java
+++ src/androidTest/java/medic/gateway/alert/WakefulServiceTest.java
@@ -8,12 +8,8 @@ import org.junit.Test;
 import android.content.Intent;
 import com.commonsware.cwac.wakeful.WakefulIntentService;
 
-
-import java.util.concurrent.TimeUnit;
-
 import medic.gateway.alert.test.DbTestHelper;
 import medic.gateway.alert.test.HttpTestHelper;
-import okhttp3.mockwebserver.RecordedRequest;
 
 import static medic.gateway.alert.test.DbTestHelper.cols;
 import static medic.gateway.alert.test.DbTestHelper.randomUuid;
@@ -21,56 +17,80 @@ import static medic.gateway.alert.test.DbTestHelper.vals;
 import static medic.gateway.alert.test.TestUtils.A_PHONE_NUMBER;
 import static medic.gateway.alert.test.TestUtils.SOME_CONTENT;
 import static medic.gateway.alert.WoMessage.Status.UNSENT;
-import static org.junit.Assert.*;
 
 public class WakefulServiceTest extends AndroidTestCase {
+	private DbTestHelper db;
+	private HttpTestHelper http;
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
 
-    private DbTestHelper db;
-    private HttpTestHelper http;
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
+		db = new DbTestHelper(getContext());
 
-        db = new DbTestHelper(getContext());
+		http = new HttpTestHelper();
+		http.configureAppSettings(getContext());
+	}
 
-        http = new HttpTestHelper();
-        http.configureAppSettings(getContext());
-    }
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
 
-    @After
-    public void tearDown() throws Exception {
-        super.tearDown();
+		http.tearDown();
+		db.tearDown();
 
-        http.tearDown();
-        db.tearDown();
+		http.assertNoMoreRequests();
 
-        http.assertNoMoreRequests();
+	}
+	@Test
+	public void test_doWakefulWork_unsentMessagesShouldBeEmptyIfOnlyTenMessagesAvailable() throws Exception {
+		// given
+		db.insert("wo_message",
+				cols("_id", "status",  "last_action", "_to",   "content"),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT));
+		http.nextResponseJson("{}");
 
-    }
+		// when
+		Intent i = new Intent(getContext(), WakefulIntentService.class);
+		WakefulService wfs = new WakefulService();
+		wfs.doWakefulWork(i);
 
-    @Test
-    public void test_doWakefulWork_runPollWebAppAgainIfMessagesAvailable() throws Exception {
-        // given
-        db.insert("wo_message",
-                cols("_id", "status",  "last_action", "_to",   "content"),
-                vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
-                vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
-                vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
-                vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
-                vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
-                vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
-                vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
-                vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
-                vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
-                vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT));
-        http.nextResponseJson("{}");
+		//then
+		db.assertEmpty("wo_message");
+	}
+	@Test
+	public void test_doWakefulWork_unsentMessagesCountShouldBeEqualToTwoIfUnsentMessagesAreTwelve() {
+		// given
+		db.insert("wo_message",
+				cols("_id", "status",  "last_action", "_to",   "content"),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT),
+				vals(randomUuid(),  UNSENT,     0,     A_PHONE_NUMBER, SOME_CONTENT));
+		http.nextResponseJson("{}");
 
-        // when
-        Intent i = new Intent(getContext(), WakefulIntentService.class);
-        WakefulService wfs = new WakefulService();
-        wfs.doWakefulWork(i);
+		// when
+		Intent i = new Intent(getContext(), WakefulIntentService.class);
+		WakefulService wfs = new WakefulService();
+		wfs.doWakefulWork(i);
 
-        //then
-        db.assertEmpty("wo_message");
-    }
+		//then
+		db.assertCount("wo_message",2);
+	}
 }
