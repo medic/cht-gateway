7155877e37bec29cd8853b114c2fef275aac884c
Stefan du Fresne <stefan@sdufresne.info>
2019-11-07 17:12:02 +0000

    My attempt

--- src/main/java/medic/gateway/alert/WakefulService.java
+++ src/main/java/medic/gateway/alert/WakefulService.java
@@ -17,6 +17,10 @@ public class WakefulService extends WakefulIntentService {
 		super("WakefulService");
 	}
 
+	protected WebappPoller getWebappPoller() {
+		return new WebappPoller(this);
+	}
+
 	@SuppressWarnings({ "PMD.AvoidDeeplyNestedIfStmts", "PMD.StdCyclomaticComplexity", "PMD.ModifiedCyclomaticComplexity", "PMD.NPathComplexity" })
 	public void doWakefulWork(Intent intent) {
 		boolean enableWifiAfterWork = false;
@@ -29,31 +33,40 @@ public class WakefulService extends WakefulIntentService {
 		}
 
 		try {
-			WebappPoller poller = new WebappPoller(this);
-			SimpleResponse lastResponse = poller.pollWebapp();
+			WebappPoller poller;
+			do {
+				poller = getWebappPoller();
+				SimpleResponse lastResponse = poller.pollWebapp();
 
-			// TODO check if we should be handling other failures in addition to timeouts e.g. java.net.SocketException
-			if(lastResponse instanceof ExceptionResponse) {
-				ExceptionResponse exResponse = (ExceptionResponse) lastResponse;
-				if(exResponse.ex instanceof SocketTimeoutException ||
-						exResponse.ex instanceof UnknownHostException ||
-						exResponse.ex instanceof ConnectException ||
-						exResponse.ex instanceof NoRouteToHostException) {
-					wifiMan = new WifiConnectionManager(this);
-					if(wifiMan.isWifiActive()) {
-						logEvent(this, "Disabling wifi and then retrying poll...");
-						enableWifiAfterWork = true;
-						wifiMan.disableWifi();
-						lastResponse = poller.pollWebapp();
+				// TODO check if we should be handling other failures in addition to timeouts e.g. java.net.SocketException
+				if(lastResponse instanceof ExceptionResponse) {
+					ExceptionResponse exResponse = (ExceptionResponse) lastResponse;
+					if(exResponse.ex instanceof SocketTimeoutException ||
+							exResponse.ex instanceof UnknownHostException ||
+							exResponse.ex instanceof ConnectException ||
+							exResponse.ex instanceof NoRouteToHostException) {
+						wifiMan = new WifiConnectionManager(this);
+						if(wifiMan.isWifiActive()) {
+							logEvent(this, "Disabling wifi and then retrying poll...");
+							enableWifiAfterWork = true;
+							wifiMan.disableWifi();
+							lastResponse = poller.pollWebapp();
+						}
 					}
 				}
-			}
 
-			if(lastResponse == null || lastResponse.isError()) {
-				LastPoll.failed(this);
-			} else {
-				LastPoll.succeeded(this);
-			}
+				if(lastResponse == null || lastResponse.isError()) {
+					LastPoll.failed(this);
+				} else {
+					LastPoll.succeeded(this);
+				}
+
+			/**
+			 * NB: this is only testing that *we* have more to send to webapp, and not that webapp has
+			 * more to send to us. To enable that feature correctly we should have webapp pass us this
+			 * value back, because otherwise we'd have to hardcode webapp's batch size in Gateway
+			 */
+			} while (poller.moreMessagesToSend());
 		} catch(Exception ex) {
 			logException(this, ex, "Exception caught trying to poll webapp: %s", ex.getMessage());
 			LastPoll.failed(this);
--- src/main/java/medic/gateway/alert/WebappPoller.java
+++ src/main/java/medic/gateway/alert/WebappPoller.java
@@ -61,6 +61,13 @@ public class WebappPoller {
 		return response;
 	}
 
+	public Boolean moreMessagesToSend() {
+		// Complication: we could change db calls to get MAX+1, and use that
+		// to determine if there are *truly* more
+		return request.wtMessageCount() == MAX_WT_MESSAGES ||
+					request.statusUpdateCount() == MAX_WOM_STATUS_UPDATES;
+	}
+
 //> PRIVATE HELPERS
 	private void handleOkResponse(GatewayRequest request, JSONObject response) throws JSONException {
 		for(WtMessage m : request.messages) {
